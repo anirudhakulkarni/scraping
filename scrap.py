3d1021cdb40f948985e44f94047127b03450d225
ANIRUDHA KULKARNI <kulkarnianirudha8@gmail.com>
2020-04-04 08:11:24 +0530

    download option added

--- scrap.py
+++ scrap.py
@@ -1,10 +1,12 @@
 from bs4 import BeautifulSoup
 import requests
 import os
+import urllib.request
 
 source = requests.get('http://explosm.net/comics/archive').text
 soup = BeautifulSoup(source, 'lxml')
 
+starting= soup.find('')
 #input syntx
 with open('range.txt') as f:
     lines=[i.rstrip('\n')for i in f]
@@ -12,3 +14,10 @@ with open('range.txt') as f:
 (end_month,end_year)=lines[1].split()
 author=lines[2]
 
+#output
+# for i in outputfile:
+#     path= 'C:/Users/Admin/Desktop/scrapping/output'+str(start_year)+'/'+start_month
+#     os.makedirs(path)
+imagefile=open('name'+'.jpeg','wb')
+imagefile.write(urllib.request.urlopen('http://files.explosm.net/comics/Kris/fightcloud.png?t=C1CB9B').read())
+imagefile.close()
